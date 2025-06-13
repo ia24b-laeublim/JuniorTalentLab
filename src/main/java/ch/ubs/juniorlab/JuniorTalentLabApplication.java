9b5d775e08be105f3e97346cc4c2662c64b61c98
Zoran <varnicicz@bzz.ch>
2025-06-13 15:40:26 +0200

    :)

--- src/main/java/ch/ubs/juniorlab/JuniorTalentLabApplication.java
+++ src/main/java/ch/ubs/juniorlab/JuniorTalentLabApplication.java
@@ -1,6 +1,6 @@
 package ch.ubs.juniorlab;
 
-import ch.ubs.juniorlab.service.TaskProcessingService;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
