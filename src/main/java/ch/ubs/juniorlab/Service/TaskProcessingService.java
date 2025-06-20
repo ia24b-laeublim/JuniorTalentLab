c711902702cdc59b3c9e68d5f2af72da182f514e
Zoran <varnicicz@bzz.ch>
2025-06-20 08:25:31 +0200

    :)

--- src/main/java/ch/ubs/juniorlab/Service/TaskProcessingService.java
+++ src/main/java/ch/ubs/juniorlab/Service/TaskProcessingService.java
@@ -27,12 +27,12 @@ public class TaskProcessingService {
             System.out.println("Beschreibung: " + task.getDescription());
 
             if (task instanceof FlyerTask flyer) {
-                System.out.println("➡️ FlyerTask");
+                System.out.println("FlyerTask");
                 System.out.println("Papiergrösse: " + flyer.getPaperSize());
                 System.out.println("Papiertyp: " + flyer.getPaperType());
 
             } else if (task instanceof PosterTask poster) {
-                System.out.println("➡️ PosterTask");
+                System.out.println("PosterTask");
                 System.out.println("Format: " + poster.getFormat());
                 System.out.println("Postergrösse: " + poster.getPosterSize());
                 System.out.println("Papiertyp: " + poster.getPaperType());
@@ -40,14 +40,14 @@ public class TaskProcessingService {
                 System.out.println("Montageart: " + poster.getMountingType());
 
             } else if (task instanceof PhotoTask photo) {
-                System.out.println("➡️ PhotoTask");
+                System.out.println("PhotoTask");
                 System.out.println("Format: " + photo.getFormat());
                 System.out.println("Dateiformat: " + photo.getFileFormat());
                 System.out.println("Plattformen: " + photo.getSocialMediaPlatforms());
                 System.out.println("Auflösung: " + photo.getResolution());
 
             } else if (task instanceof PollTask poll) {
-                System.out.println("➡️ PollTask");
+                System.out.println("PollTask");
                 System.out.println("Umfragetitel: " + poll.getTitle());
                 System.out.println("Fragenanzahl: " + poll.getQuestionCount());
                 System.out.println("Fragetyp: " + poll.getQuestionType());
@@ -55,7 +55,7 @@ public class TaskProcessingService {
                 System.out.println("Verteilungsmethode: " + poll.getDistributionMethod());
 
             } else if (task instanceof SlideshowTask slideshow) {
-                System.out.println("➡️ SlideshowTask");
+                System.out.println("SlideshowTask");
                 System.out.println("Format: " + slideshow.getFormat());
                 System.out.println("Dateiformat: " + slideshow.getFileFormat());
                 System.out.println("Plattformen: " + slideshow.getSocialMediaPlatforms());
@@ -63,7 +63,7 @@ public class TaskProcessingService {
                 System.out.println("Auflösung: " + slideshow.getResolution());
 
             } else if (task instanceof VideoTask video) {
-                System.out.println("➡️ VideoTask");
+                System.out.println("VideoTask");
                 System.out.println("Länge (Sekunden): " + video.getLengthSec());
                 System.out.println("Voiceover: " + video.getVoiceover());
                 System.out.println("Disclaimer: " + video.getDisclaimer());
@@ -75,10 +75,8 @@ public class TaskProcessingService {
                 System.out.println("Musikstil: " + video.getMusicStyle());
 
             } else {
-                System.out.println("⚠️ Unbekannter Task-Typ: " + task.getClass().getSimpleName());
+                System.out.println("Unbekannter Task-Typ: " + task.getClass().getSimpleName());
             }
-
-            System.out.println("----------------------");
         }
     }
 
