9278079e99da337e659587cc97e68aaebd1d39d3
Zoran <varnicicz@bzz.ch>
2025-07-11 14:41:49 +0200

    Neuer Controller versuche Den file Upload zu machen

---
+++ src/main/java/ch/ubs/juniorlab/controller/FileUploadController.java
@@ -0,0 +1,18 @@
+package ch.ubs.juniorlab.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.nio.file.*;
+
+@Controller
+public class FileUploadController {
+
+
+
+}
